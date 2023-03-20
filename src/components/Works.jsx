184c36eb31f642d7c6c287d28f998a562dd57d08
Irfan <im4941668@gmail.com>
2023-03-20 11:56:17 +0530

    blog add

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -78,7 +78,7 @@ const Works = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-7"
       >
-        Following projects showcases my skills and experience through real-world
+        Following projects showcase my skills and experience through real-world
         examples of my work. Each project is briefly described with links to
         code repositories and live demos in it. It reflects my ability to solve
         complex problems, work with different technologies, and manage projects
